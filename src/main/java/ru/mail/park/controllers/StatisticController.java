9a7324c11a7af65159a1d0f34c819e381920344c
Liza Pyanova <eliza2607@mail.ru>
2016-12-15 22:25:41 +0300

    StatisticController modify

--- src/main/java/ru/mail/park/controllers/StatisticController.java
+++ src/main/java/ru/mail/park/controllers/StatisticController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ru.mail.park.controllers.api.common.ResultJson;
 import ru.mail.park.model.user.UserProfile;
@@ -33,7 +34,7 @@ public class StatisticController {
 
     @RequestMapping(path = "/user/rating", method = RequestMethod.GET,
             produces = "application/json")
-    public ResponseEntity getWinRate(UserProfile userProfile) {
+    public ResponseEntity getWinRate(@RequestParam(name = "email") String email) {
 
 //        final String email = sessionService.getAuthorizedEmail(httpSession.getId());
 
@@ -41,11 +42,25 @@ public class StatisticController {
 //            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new ResultJson<>(
 //                    HttpStatus.UNAUTHORIZED.value(), HttpStatus.UNAUTHORIZED).getStringResult());
 
-        userProfile = accountService.getUser(userProfile.getEmail());
+        final String json;
 
-        Float winRate = (float)userProfile.getWinGames() / userProfile.getCountGames();
+        if(email == null) {
+            json = (new ResultJson<>(
+                    HttpStatus.FORBIDDEN.value(), null).getStringResult());
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(json);
+        }
 
-        final String json = (new ResultJson<>(
+        UserProfile userProfile = accountService.getUser(email);
+
+        if(userProfile == null) {
+            json = (new ResultJson<>(
+                    HttpStatus.FORBIDDEN.value(), null).getStringResult());
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(json);
+        }
+
+        Float winRate = (float)userProfile.getWinGames() / (float)userProfile.getCountGames();
+
+        json = (new ResultJson<>(
                 HttpStatus.OK.value(), winRate.floatValue()).getStringResult());
 
         return ResponseEntity.status(HttpStatus.OK).body(json);
